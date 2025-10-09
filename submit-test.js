161b4bfce663dcdbff9c112671016f0e023f5af5
Bob Webb <bob.wbb@gmail.com>
2025-10-09 15:36:34 +0200

    Test for the texts argument

--- submit-test.js
+++ submit-test.js
@@ -1,38 +1,44 @@
 import { parse } from 'csv-parse/sync';
 
+// the tweet analysis proxy
 const localUrl = 'http://localhost:8787';
 
-const serverUrl = 'https://tweet-analysis-worker.bob-wbb.workers.dev';
+// const serverUrl = 'https://tweet-analysis-worker.bob-wbb.workers.dev';
 
 import fs from 'fs';
 
 const baselineData = parse(fs.readFileSync('../openrouter-experiment/baseline-10000.csv', 'utf-8'));
 console.log(baselineData);
 
-for (const numTweets of [1, 2, 5, 10, 20, 50]) {
+for (const numTweets of [10]) {
 	console.log(`Evaluating batch with ${numTweets} tweets`);
 
 	for (let i = 0; i < 5; i++) {
-		const texts = baselineData.slice(0, numTweets).map((row) => row[1]);
+		try {
+			const texts = baselineData.slice(i * numTweets, numTweets).map((row) => row[1]);
 
-		const model = 'mistralai/Mistral-Small-3.2-24B-Instruct-2506';
+			const model = 'mistralai/Mistral-Small-3.2-24B-Instruct-2506';
 
-		const startTime = performance.now();
-		const response = await fetch(localUrl, {
-			method: 'POST',
-			body: JSON.stringify({
-				texts,
-				model,
-			}),
-			headers: { 'content-type': 'application/json' },
-		});
-		const endTime = performance.now();
+			const startTime = performance.now();
+			const response = await fetch(localUrl, {
+				method: 'POST',
+				body: JSON.stringify({
+					texts,
+					model,
+				}),
+				headers: { 'content-type': 'application/json' },
+			});
+			const endTime = performance.now();
 
-		const responseData = await response.json();
+			const responseData = await response.json();
 
-		// console.log(responseData.choices[0].message.content);
+			console.log(responseData.choices[0].message.content);
 
-		const timeTakenMs = endTime - startTime;
-		console.log(`${numTweets} tweets processed in  ${timeTakenMs}ms, ${timeTakenMs / numTweets}ms per tweet`);
+			const timeTakenMs = endTime - startTime;
+			console.log(`${numTweets}, ${timeTakenMs}, ${timeTakenMs / numTweets}`);
+			// console.log(`${numTweets} tweets processed in  ${timeTakenMs}ms, ${timeTakenMs / numTweets}ms per tweet`);
+		} catch (e) {
+			console.log(e);
+		}
 	}
 }
